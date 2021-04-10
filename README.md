408d7de2fe073937def5eca0d10394ebf122cf22
reuixiy <reuixiy@gmail.com>
2021-04-10 17:32:40 +0800

    feat: add a basic panel and adapt to mobile

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # cool-alien-art
 
+## Preview
+
 [![cool-alien-art.png](cool-alien-art.png)](https://io-oi.me/cool-alien-art)
 
-ref: https://twitter.com/CodigoFalado/status/1379450845599784962
+## Acknowledgments
+
+- https://twitter.com/aemkei/status/1378106731386040322
+- https://twitter.com/CodigoFalado/status/1379450845599784962
--- cool-alien-art.js
+++ cool-alien-art.js
@@ -1,6 +1,8 @@
 const canvas = document.createElement('canvas');
 
-function draw() {
+const pattern = '((x - 128) * 64) % (y - 128)';
+
+function draw(patternInput) {
   const width = (canvas.width = window.innerWidth);
   const height = (canvas.height = window.innerHeight);
 
@@ -8,7 +10,7 @@ function draw() {
 
   for (let x = 0; x < 256; x++) {
     for (let y = 0; y < 256; y++) {
-      if (((x - 128) * 64) % (y - 128)) {
+      if (eval(patternInput || pattern)) {
         ctx.fillStyle = `hsl(${y}, 100%, 50%)`;
         ctx.fillRect(
           x * 4 * (width / 1024),
@@ -25,8 +27,101 @@ function draw() {
 
 window.addEventListener('DOMContentLoaded', () => {
   draw();
+
+  initUI();
 });
 
 window.addEventListener('resize', () => {
   draw();
 });
+
+function initUI() {
+  draggable(document.querySelector('aside'));
+
+  const drawButton = document.querySelector('#draw');
+  const downloadButton = document.querySelector('#download');
+  const textarea = document.querySelector('textarea');
+
+  drawButton.addEventListener('click', () => {
+    draw(textarea.value);
+  });
+
+  textarea.addEventListener('mousedown', (e) => {
+    e.stopPropagation();
+  });
+
+  downloadButton.addEventListener('click', () => {
+    const a = document.createElement('a');
+
+    a.download = 'cool-alien-art.png';
+    a.href = canvas.toDataURL();
+    a.target = '_blank';
+
+    a.click();
+  });
+}
+
+/**
+ * Makes an element draggable.
+ *
+ * https://gist.github.com/remarkablemark/5002d27442600510d454a5aeba370579
+ *
+ * @param {HTMLElement} element - The element.
+ */
+function draggable(element) {
+  var isMouseDown = false;
+
+  // initial mouse X and Y for `mousedown`
+  var mouseX;
+  var mouseY;
+
+  // element X and Y before and after move
+  var elementX = 0;
+  var elementY = 0;
+
+  // mouse button down over the element
+  element.addEventListener('mousedown', onMouseDown);
+
+  /**
+   * Listens to `mousedown` event.
+   *
+   * @param {Object} event - The event.
+   */
+  function onMouseDown(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    isMouseDown = true;
+  }
+
+  // mouse button released
+  element.addEventListener('mouseup', onMouseUp);
+
+  /**
+   * Listens to `mouseup` event.
+   *
+   * @param {Object} event - The event.
+   */
+  function onMouseUp(event) {
+    isMouseDown = false;
+    elementX = parseInt(element.style.left) || 0;
+    elementY = parseInt(element.style.top) || 0;
+  }
+
+  // need to attach to the entire document
+  // in order to take full width and height
+  // this ensures the element keeps up with the mouse
+  document.addEventListener('mousemove', onMouseMove);
+
+  /**
+   * Listens to `mousemove` event.
+   *
+   * @param {Object} event - The event.
+   */
+  function onMouseMove(event) {
+    if (!isMouseDown) return;
+    var deltaX = event.clientX - mouseX;
+    var deltaY = event.clientY - mouseY;
+    element.style.left = elementX + deltaX + 'px';
+    element.style.top = elementY + deltaY + 'px';
+  }
+}
